dae0ed2a982e3bb4a6f16a75b16354ca0c98323d
Rafael Castro <ruevko@proton.me>
2025-11-06 11:54:46 -0500

    Move joins to renderTable()

--- apps/identificar-punto/server.R
+++ apps/identificar-punto/server.R
@@ -1,15 +1,16 @@
 function(input, output, session) {
     # ejecutar al presionar el botón IDENTIFICAR
-    simple_data = eventReactive(input$identify,
+    simple_point = eventReactive(input$identify,
         tryCatch( {
             # cancelar ejecución si ningún accordion está desplegado
             if(is.null(input$method)) stop("Seleccione un método de identificación.")
 
             # alternar el método según el accordion desplegado
-            point = switch(input$method,
+            switch(input$method,
                 # crear un punto con las coordenadas ingresadas
                 "POR COORDENADAS" = validate_coordinates(input$text1, input$text2),
 
+                # obtener el punto con la ubicación del dispositivo
                 "POR UBICACIÓN" = get_device_coordinates(session, input),
 
                 # extraer el punto de una fotografía georreferenciada
@@ -17,44 +18,43 @@ function(input, output, session) {
                     extract_photo_coordinates(input$file1$datapath)
                 } else stop("No se ha cargado una fotografía.")
             )
-
-            # unir datos de las coberturas al punto
-            point = point |>
-                st_join(BARRIO_SECTOR, st_within) |>
-                st_join(PUGS_ESTRUCTURANTE, st_within) |>
-                st_join(PUGS_USO_SUELO, st_within)
-
-            point_geo = paste(
-                c("Latitud:", "Longitud:"), collapse = ", ",
-                st_coordinates(point) |> round(5) |> rev()
-            )
-
-            # calcular coordenadas UTM del punto
-            point_utm = paste(
-                c("Este:", "Norte:"), collapse = ", ",
-                st_coordinates(st_transform(point, crs = UTM_EPSG)) |> round(0)
-            )
-
-            point_utm_zone = paste0(UTM_EPSG %% 100, ifelse(UTM_EPSG > 32700, "S", "N"))
-
-            # preparar tabla para la pestaña DATOS
-            dplyr::tribble(
-                ~key,                                          ~value,
-                "Coordenadas geográficas",                     point_geo,
-                paste("Coordenadas UTM Zona", point_utm_zone), point_utm,
-                "Parroquia",                                   point$parroquia,
-                "Sector",                                      point$sector,
-                "Barrio",                                      point$barrio,
-                "Clasificación (PUGS 2025)",                   point$clasificac,
-                "Subclasificación (PUGS 2025)",                point$subclasifi,
-                "Uso general del suelo",                       point$uso_genera,
-                "Uso específico del suelo",                    point$uso_especi
-            ) },
-            # capturar error, en caso de ocurrir
-            error = function(error) paste("Error:", error$message)
-        )
+        # capturar error, en caso de ocurrir
+        }, error = function(error) stop(safeError(error$message)))
     )
 
     # renderizar tabla
-    output$simple_data = renderTable(simple_data(), border = TRUE)
+    output$simple_data = renderTable( {
+        # unir datos de las coberturas al punto
+        point = simple_point() |>
+            st_join(BARRIO_SECTOR, st_within) |>
+            st_join(PUGS_ESTRUCTURANTE, st_within) |>
+            st_join(PUGS_USO_SUELO, st_within)
+
+        point_geo = paste(
+            c("Latitud:", "Longitud:"), collapse = ", ",
+            st_coordinates(point) |> round(5) |> rev()
+        )
+
+        # calcular coordenadas UTM del punto
+        point_utm = paste(
+            c("Este:", "Norte:"), collapse = ", ",
+            st_coordinates(st_transform(point, crs = UTM_EPSG)) |> round(0)
+        )
+
+        point_utm_zone = paste0(UTM_EPSG %% 100, ifelse(UTM_EPSG > 32700, "S", "N"))
+
+        # preparar tabla para la pestaña DATOS
+        dplyr::tribble(
+            ~key,                                          ~value,
+            "Coordenadas geográficas",                     point_geo,
+            paste("Coordenadas UTM Zona", point_utm_zone), point_utm,
+            "Parroquia",                                   point$parroquia,
+            "Sector",                                      point$sector,
+            "Barrio",                                      point$barrio,
+            "Clasificación (PUGS 2025)",                   point$clasificac,
+            "Subclasificación (PUGS 2025)",                point$subclasifi,
+            "Uso general del suelo",                       point$uso_genera,
+            "Uso específico del suelo",                    point$uso_especi
+        )
+    }, border = TRUE)
 }
